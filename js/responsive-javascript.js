f56070b3f4b888bf789c47d2aed7668d3996c6c5
Travis Mullen <travis@boombangmedia.com>
2015-07-28 13:42:01 -0400

    adding SIAF

--- js/responsive-javascript.js
+++ js/responsive-javascript.js
@@ -1,115 +1,117 @@
 "use strict";
 
-var rjs = {
+(function ( global ){
+    global.rjs = {
 
-    config: {
-        name: "responsive-javascript",
-        attribTarget: "borderStyle",
-        breakpoints: {
-            small: "dashed",
-            medium: "dotted",
-            large: "double"
-        },
-        prefix: "viewport",
-        debounceTime: 30
-    },
-
-    $window: $( window ),
-    viewport: undefined,
-
-    init: function( config ) {
-        var that = this,
-            push;
-
-        if ( _.isObject( config ) ) {
-            this.config = this.setConfig( config );
-        } else if ( _.isNumber( config ) ) {
-            this.config.debounceTime = config;
-        }
-
-        // this.elm = $("[" + this.config.name + "]"); // attach to attrib
-        this.elm = $( 'body' ); // attach to body
-
-        this.target = $( '<div />' , {
-            'class': this.config.name,
-            'style': "display:none;"
-        });
-
-        this.elm.append( this.target );
-
-        this.pushTriggers();
-
-        push = _.debounce( function() {
-            that.pushTriggers();
-        }, this.config.debounceTime );
-
-        this.$window.on( 'resize' , push );
-
-    },
-    setConfig: function( config ) {
-        return {
-            name: config.name || this.config.name,
-            attribTarget: config.attribTarget || this.config.attribTarget,
+        config: {
+            name: "responsive-javascript",
+            attribTarget: "borderStyle",
             breakpoints: {
-                small: config.breakpoints && config.breakpoints.small || this.config.breakpoints.small,
-                medium: config.breakpoints && config.breakpoints.medium || this.config.breakpoints.medium,
-                large: config.breakpoints && config.breakpoints.large || this.config.breakpoints.large
+                small: "dashed",
+                medium: "dotted",
+                large: "double"
             },
-            prefix: config.prefix || this.config.prefix,
-            debounceTime: config.debounceTime || this.config.debounceTime
-        }
-    },
-    findViewPort: function( viewport ) {
-        var ports = this.config.breakpoints,
-            view;
-        for (view in ports) {
-            if (viewport === ports[view]) {
-                return view;
-            }
-        }
-    },
-    findViewExclusions: function( viewport ) {
-        var ports = this.config.breakpoints,
-            exc = [],
-            view;
-        for (view in ports) {
-            if (viewport !== ports[view]) {
-                exc.push( view );
-            }
-        }
-        return exc;
-    },
-    pushTriggers: function() {
-        var value = this.target.css( this.config.attribTarget ),
-            vp = this.findViewPort( value ),
-            excs = this.findViewExclusions( value );
+            prefix: "viewport",
+            debounceTime: 30
+        },
 
-        // for angularjs use `scope.$emit`
-        this.$window.trigger( this.config.prefix + ':' + vp + ':resize' );
-        for (var i = excs.length - 1; i >= 0; i--) {
-            this.$window.trigger( this.config.prefix + ':not:' + excs[i] + ':resize' );
-        };
+        $window: $( window ),
+        viewport: undefined,
 
-        // if viewport has changed fire trigger
-        if ( this.viewport === vp ) {
-            return;
+        init: function( config ) {
+            var that = this,
+                push;
+
+            if ( _.isObject( config ) ) {
+                this.config = this.setConfig( config );
+            } else if ( _.isNumber( config ) ) {
+                this.config.debounceTime = config;
+            }
+
+            // this.elm = $("[" + this.config.name + "]"); // attach to attrib
+            this.elm = $( 'body' ); // attach to body
+
+            this.target = $( '<div />' , {
+                'class': this.config.name,
+                'style': "display:none;"
+            });
+
+            this.elm.append( this.target );
+
+            this.pushTriggers();
+
+            push = _.debounce( function() {
+                that.pushTriggers();
+            }, this.config.debounceTime );
+
+            this.$window.on( 'resize' , push );
+
+        },
+        setConfig: function( config ) {
+            return {
+                name: config.name || this.config.name,
+                attribTarget: config.attribTarget || this.config.attribTarget,
+                breakpoints: {
+                    small: config.breakpoints && config.breakpoints.small || this.config.breakpoints.small,
+                    medium: config.breakpoints && config.breakpoints.medium || this.config.breakpoints.medium,
+                    large: config.breakpoints && config.breakpoints.large || this.config.breakpoints.large
+                },
+                prefix: config.prefix || this.config.prefix,
+                debounceTime: config.debounceTime || this.config.debounceTime
+            }
+        },
+        findViewPort: function( viewport ) {
+            var ports = this.config.breakpoints,
+                view;
+            for (view in ports) {
+                if (viewport === ports[view]) {
+                    return view;
+                }
+            }
+        },
+        findViewExclusions: function( viewport ) {
+            var ports = this.config.breakpoints,
+                exc = [],
+                view;
+            for (view in ports) {
+                if (viewport !== ports[view]) {
+                    exc.push( view );
+                }
+            }
+            return exc;
+        },
+        pushTriggers: function() {
+            var value = this.target.css( this.config.attribTarget ),
+                vp = this.findViewPort( value ),
+                excs = this.findViewExclusions( value );
+
+            // for angularjs use `scope.$emit`
+            this.$window.trigger( this.config.prefix + ':' + vp + ':resize' );
+            for (var i = excs.length - 1; i >= 0; i--) {
+                this.$window.trigger( this.config.prefix + ':not:' + excs[i] + ':resize' );
+            };
+
+            // if viewport has changed fire trigger
+            if ( this.viewport === vp ) {
+                return;
+            }
+            this.viewport = vp;
+            this.$window.trigger( this.config.prefix + ':' + vp );
+            for (var i = excs.length - 1; i >= 0; i--) {
+                this.$window.trigger( this.config.prefix + ':not:' + excs[i] );
+            };
+        },
+        isSmall: function() {
+            return this.viewport === 'small';
+        },
+        isMedium: function() {
+            return this.viewport === 'medium';
+        },
+        isLarge: function() {
+            return this.viewport === 'large';
         }
-        this.viewport = vp;
-        this.$window.trigger( this.config.prefix + ':' + vp );
-        for (var i = excs.length - 1; i >= 0; i--) {
-            this.$window.trigger( this.config.prefix + ':not:' + excs[i] );
-        };
-    },
-    isSmall: function() {
-        return this.viewport === 'small';
-    },
-    isMedium: function() {
-        return this.viewport === 'medium';
-    },
-    isLarge: function() {
-        return this.viewport === 'large';
-    }
-};
+    };
+}( this ));
 
 // rjs.init(); // and go!
 
